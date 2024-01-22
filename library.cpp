5b27ca0f9fe5443202bcf49494cda1e60796ac90
Flugle12 <148736438+Flugle12@users.noreply.github.com>
2024-01-22 14:52:01 +0300

    Update library.cpp

--- library.cpp
+++ library.cpp
@@ -1,8 +1,31 @@
-// library.cpp
+// main.cpp
 #include <iostream>
+#include <dlfcn.h>
 
-extern "C" {
-    void hello() {
-        std::cout << "Hello from the dynamic library!" << std::endl;
+typedef void (*HelloFunction)();
+
+int main() {
+    void* handle = dlopen("./-llibrary.so", RTLD_LAZY);
+    if (!handle) {
+        std::cerr << "Cannot open library: " << dlerror() << std::endl;
+        return 1;
     }
+
+    // Загрузка функции из библиотеки
+    HelloFunction hello = (HelloFunction) dlsym(handle, "hello");
+    const char *dlsym_error = dlerror();
+    if (dlsym_error) {
+        std::cerr << "Cannot load symbol 'hello': " << dlsym_error << std::endl;
+        dlclose(handle);
+        return 1;
+    }
+
+    // Вызов функции из библиотеки
+    hello();
+
+    // Закрытие библиотеки
+    dlclose(handle);
+
+    return 0;
 }
+
